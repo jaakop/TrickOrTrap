d142ae9479c98e6cf50c334151fa650554fac92b
jaakop <jaakko@kronogra.fi>
2024-11-02 01:51:39 +0200

    Clean up

--- scenes/globals/global.gd
+++ scenes/globals/global.gd
@@ -3,12 +3,3 @@ class_name  Globals
 
 var currentCharacter: Character;
 var dayTime: float;
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
