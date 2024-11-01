92776e68e007841d6d676458256980f09ab9d643
jaakop <jaakko@kronogra.fi>
2024-11-01 21:35:21 +0200

    Restructured the door

--- scenes/front_door/Door.gd
+++ scenes/front_door/Door.gd
@@ -1,7 +1,7 @@
-extends Node2D
+extends Node
 
 @export_file var characterScene:String
 
-func _on_texture_button_pressed():
-    get_tree().change_scene_to_file(characterScene);
-    pass # Replace with function body.
+func _on_window_button_pressed():
+	get_tree().change_scene_to_file(characterScene);
+	pass # Replace with function body.
