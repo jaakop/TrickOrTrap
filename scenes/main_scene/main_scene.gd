846e57b030d1a5e73167fa4367de26aadbf3fde7
jaakop <jaakko@kronogra.fi>
2024-11-01 21:42:27 +0200

    Logic for handling different characters

--- scenes/main_scene/main_scene.gd
+++ scenes/main_scene/main_scene.gd
@@ -11,3 +11,26 @@ func _ready():
 
 func _process(delta):
     pass;
+
+func _on_light_switch_pressed():
+    if(global.currentCharacter.evil):
+        print("Evil character yeeted!");
+    else:
+        print("Good guy yeeted :C");
+    generateNewCharacter();
+    pass # Replace with function body.
+
+func _on_handle_pressed():
+    if(global.currentCharacter.evil):
+        print("Evil character let in :c");
+    else:
+        print("Good guy let in!");
+    generateNewCharacter();
+    pass # Replace with function body.
+
+func generateNewCharacter():
+    global.currentCharacter.generated = false;
+    global.currentCharacter.evil = randi_range(0,1) == 1;
+    global.currentCharacter.faceTexture = "";
+    global.currentCharacter.bodyTexture = "";
+    pass;
